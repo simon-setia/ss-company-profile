5e4d72bbac8c025facc117cfae39cdbe4964bcd7
simon-setia <s.setia@bsp-groups.com>
2017-06-06 23:30:19 +0700

    First Push

---
+++ Company.Profile/Company.Profile/Controllers/HomeController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Company.Profile.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public String Index()
+        {
+            return "This my first hello ASP.Net MVC";
+        }
+    }
+}
